567e2b3d29c411df72cf7255528a3250d4532ada
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-07-05 21:29:51 +0700

    add method getAndCachePostCities

--- app/functions.php
+++ app/functions.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Enums\SystemCacheKeyEnum;
 use App\Enums\UserRoleEnum;
+use App\Models\Post;
 
 if (!function_exists('getRoleByKey')) {
     function getRoleByKey($key): string
@@ -13,5 +15,34 @@ if (!function_exists('user')) {
     function user(): ?object
     {
         return auth()->user();
+    }
+}
+
+if (!function_exists('getAndCachePostCities')) {
+    function getAndCachePostCities(): array
+    {
+        return cache()->remember(
+            SystemCacheKeyEnum::POST_CITIES,
+            86400 * 30,
+            function () {
+                $cities  = Post::query()
+                    ->pluck('city');
+                $arrCity = [];
+                foreach ($cities as $city) {
+                    if (empty($city)) {
+                        continue;
+                    }
+                    $arr = explode(', ', $city);
+                    foreach ($arr as $item) {
+                        if (in_array($item, $arrCity)) {
+                            continue;
+                        }
+                        $arrCity[] = $item;
+                    }
+                }
+
+                return $arrCity;
+            }
+        );
     }
 }
