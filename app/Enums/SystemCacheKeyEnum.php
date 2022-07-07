f2f01e056e6d1891d796d8c226ab56547525df62
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-07-07 22:07:51 +0700

    update(get and cache): config

--- app/Enums/SystemCacheKeyEnum.php
+++ app/Enums/SystemCacheKeyEnum.php
@@ -7,4 +7,5 @@ use BenSampo\Enum\Enum;
 final class SystemCacheKeyEnum extends Enum
 {
     public const POST_CITIES = 'post_cities';
+    public const CONFIGS = 'configs';
 }
--- app/Models/Config.php
+++ app/Models/Config.php
@@ -2,10 +2,44 @@
 
 namespace App\Models;
 
+use App\Enums\SystemCacheKeyEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Config extends Model
 {
     use HasFactory;
+
+    public static function getAndCache($isPublic): array
+    {
+        return cache()->remember(
+            SystemCacheKeyEnum::CONFIGS . $isPublic,
+            86400 * 30,
+            function () use ($isPublic) {
+                $data = self::query()
+                    ->where('is_public', $isPublic)
+                    ->get();
+                $arr  = [];
+
+                foreach ($data as $each) {
+                    $arr[$each->key] = $each->value;
+                }
+
+                return $arr;
+            }
+        );
+    }
+
+    public static function getByKey($key)
+    {
+        return cache()->remember(
+            SystemCacheKeyEnum::CONFIGS . $key,
+            86400 * 30,
+            function () use ($key) {
+                return self::query()
+                    ->where('key', $key)
+                    ->value('value');
+            }
+        );
+    }
 }
