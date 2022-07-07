bb16878ebada67b2408d0a2f880a3035a23b4fb1
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-07-07 22:05:51 +0700

    update langs

--- resources/lang/en/frontpage.php
+++ resources/lang/en/frontpage.php
@@ -3,4 +3,6 @@
 return array (
   'title' => 'Find what you need',
   'location' => 'Location',
+  'from_salary' => 'From',
+  'to_salary' => 'Up to',
 );
--- resources/lang/vi/frontpage.php
+++ resources/lang/vi/frontpage.php
@@ -3,4 +3,6 @@
 return array (
   'title' => 'Tìm thứ bạn cần',
   'location' => 'Địa điểm',
+  'from_salary' => 'Trên',
+  'to_salary' => 'Tới',
 );
