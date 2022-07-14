062484fa97871f47683b85572adb8c64d71592c5
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-07-14 22:08:00 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,7 @@ use App\Enums\PostCurrencySalaryEnum;
 use App\Enums\PostRemotableEnum;
 use App\Enums\PostStatusEnum;
 use App\Enums\SystemCacheKeyEnum;
+use Carbon\Carbon;
 use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -15,6 +16,72 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use Illuminate\Support\Str;
 
+/**
+ * App\Models\Post
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int|null $company_id
+ * @property string $job_title
+ * @property string|null $district
+ * @property string|null $city
+ * @property int|null $remotable
+ * @property int|null $can_parttime
+ * @property float|null $min_salary
+ * @property float|null $max_salary
+ * @property int|null $currency_salary
+ * @property string|null $requirement
+ * @property string|null $start_date
+ * @property string|null $end_date
+ * @property int|null $number_applicants
+ * @property int $status
+ * @property int $is_pinned
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read \App\Models\Company|null $company
+ * @property-read \App\Models\File|null $file
+ * @property-read string $currency_salary_code
+ * @property-read string|null $location
+ * @property-read string $remotable_name
+ * @property-read string $salary
+ * @property-read string $status_name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Language[] $languages
+ * @property-read int|null $languages_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Post approved()
+ * @method static \Database\Factories\PostFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post findSimilarSlugs(string $attribute, array $config, string
+ *     $slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereCanParttime($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereCompanyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereCurrencySalary($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereDistrict($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereEndDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereIsPinned($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereJobTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereMaxSalary($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereMinSalary($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereNumberApplicants($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereRemotable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereRequirement($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereStartDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Post
+ *     withUniqueSlugConstraints(\Illuminate\Database\Eloquent\Model $model, string $attribute, array $config, string
+ *     $slug)
+ * @mixin \Eloquent
+ */
 class Post extends Model
 {
     use HasFactory;
@@ -42,12 +109,16 @@ class Post extends Model
     // protected $appends = [
     //     'currency_salary_code',
     // ];
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date'   => 'date',
+    ];
 
     protected static function booted(): void
     {
         static::creating(static function ($object) {
             $object->user_id = user()->id;
-            $object->status  = 1;
+            $object->status  = PostStatusEnum::getByRole();
         });
         static::saved(static function ($object) {
             $city    = $object->city;
@@ -155,5 +226,22 @@ class Post extends Model
         }
 
         return '';
+    }
+
+    public function scopeApproved($query)
+    {
+        return $query->where('status', PostStatusEnum::ADMIN_APPROVED);
+    }
+
+    public function getIsNotAvailableAttribute(): bool
+    {
+        if (empty($this->start_date)) {
+            return false;
+        }
+        if (empty($this->end_date)) {
+            return false;
+        }
+
+        return !now()->between($this->start_date, $this->end_date);
     }
 }
