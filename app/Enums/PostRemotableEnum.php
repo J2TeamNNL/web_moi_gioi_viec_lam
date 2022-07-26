60c02420d1303a48228d99bcf507769c2e575e34
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-07-26 21:47:07 +0700

    Update PostRemotableEnum.php

--- app/Enums/PostRemotableEnum.php
+++ app/Enums/PostRemotableEnum.php
@@ -6,14 +6,15 @@ use BenSampo\Enum\Enum;
 
 final class PostRemotableEnum extends Enum
 {
-    public const REMOTE_ONLY = 1;
+    public const ALL = 0;
     public const OFFICE_ONLY = 2;
     public const HYBRID = 3;
+    public const REMOTE_ONLY = 1;
 
     public static function getArrWithLowerKey(): array
     {
+        $arr  = [];
         $data = self::asArray();
-        $arr  = ['all' => 0];
 
         foreach ($data as $key => $val) {
             $index       = strtolower($key);
@@ -21,5 +22,13 @@ final class PostRemotableEnum extends Enum
         }
 
         return $arr;
+    }
+
+    public static function getArrWithoutAll(): array
+    {
+        $array = self::asArray();
+        array_shift($array);
+
+        return $array;
     }
 }
