5082ab9f03755e28d4853f5505ed071c5b7de152
Nguyễn Nam Long <j2teamnnl@gmail.com>
2022-08-16 21:25:26 +0700

    Create PostLevelEnum.php

---
+++ app/Enums/PostLevelEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+final class PostLevelEnum extends Enum
+{
+    public const FRESHER = '1';
+    public const JUNIOR = '2';
+    public const SENIOR = '3';
+    public const LEADER = '4';
+    public const LECTURER = '5';
+    public const MANAGER = '6';
+}
